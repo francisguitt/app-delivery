855fd1a16d77f1ff25cad323e77b7c13289faa0b
Guitt Zoom <77627405+francisguitt@users.noreply.github.com>
2024-03-15 18:24:51 -0300

    Update script.js

--- food-delivery/script.js
+++ food-delivery/script.js
@@ -185,7 +185,7 @@ checkoutBtn.addEventListener("click", function(){
   }).join("")
 
   const message = encodeURIComponent(cartItems)
-  const phone = "5511977838504"
+  const phone = ""
 
   window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
 
